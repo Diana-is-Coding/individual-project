a9847293b483444498e5b252fd38edd579dd3912
Diana Labode <dt.lab1991@gmail.com>
2024-07-08 15:32:23 +0000

    created views and urls, tested them to ensure they are working

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1 style= 'color: teal;'>This is the Index Page! Well Done!!</h1>")
+
+def staff(request):
+    return HttpResponse('This is the Staff Page! Doing Good!!')
