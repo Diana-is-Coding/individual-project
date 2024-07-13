c98dedc42c69e3f2867ddb12b5233e4ece486e4b
Diana Labode <dt.lab1991@gmail.com>
2024-07-13 21:05:40 +0000

    changed staff to users to match my specific project

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'dashboard/index.html')
 
-def staff(request):
-    return render (request, 'dashboard/staff.html')
+def users(request):
+    return render (request, 'dashboard/users.html')
